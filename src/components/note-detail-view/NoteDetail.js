b57f68d4a2a2353b5e6fbba233f3274920e2158f
Stas <stasscript@gmail.com>
2021-10-26 19:06:48 +0300

    Added copy to clipboard & some styles for detail page

--- src/components/note-detail-view/NoteDetail.js
+++ src/components/note-detail-view/NoteDetail.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { withRouter } from 'react-router';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import './NoteDetail.css';
 
 function NoteDetail({ match }) {
   const id = match.params.id;
   const { title, text, date } = useSelector((state) => state.notes[id]);
+  const [copyText, setCopyText] = useState('');
+
   return (
     <div className="note-detail-view">
       <div className="note-detail__content">
         <div className="note-detail__title">{title}</div>
-        <div className="note-detail__text">{text}</div>
-        <div className="note-detail__date">{date}</div>
+        <div className="note-detail__text">
+          {/* <input type="text" value={text} /> */}
+          {text}
+        </div>
       </div>
       <div className="note-detail__tools">
         <button>Edit note</button>
-        <button>Copy to clipboard</button>
+        <CopyToClipboard text={text}>
+          <button
+            onClick={() => {
+              setCopyText('Copied');
+              setTimeout(() => setCopyText(''), 2000);
+            }}>
+            Copy to clipboard
+          </button>
+        </CopyToClipboard>
+        <div className="copy__text">{copyText}</div>
         <button>Share</button>
+        <div className="note-detail__date">{date}</div>
       </div>
     </div>
   );
--- src/store/initialState.js
+++ src/store/initialState.js
@@ -12,8 +12,14 @@ const initialState = {
       date: '13 August 2021',
       id: 1,
     },
+    {
+      title: 'Lorem ipsum',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae incidunt cupiditate consectetur magnam quod praesentium quibusdam debitis. Deserunt optio nemo debitis, unde ea labore accusamus doloremque, impedit placeat voluptates amet non ullam! Mollitia voluptatibus molestias nam magni? Nemo dignissimos ratione illum labore voluptatum eveniet rem nulla dolorem asperiores deleniti doloribus, laudantium explicabo quia quasi odio facilis praesentium mollitia ducimus, distinctio numquam quisquam aliquid laborum? Qui odio distinctio repudiandae tempora ea officiis, dolore fuga impedit magni! Similique eveniet qui earum quibusdam nesciunt quasi in consectetur, minima beatae natus officia! Eaque laboriosam temporibus mollitia suscipit error aliquam expedita amet reprehenderit reiciendis quibusdam?',
+      date: '13 August 2021',
+      id: 2,
+    },
   ],
-  notesCounter: 2,
+  notesCounter: 3,
 };
 
 export default initialState;
